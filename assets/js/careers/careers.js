55c0b797fd3da9875cf3ccadfef247dc1b46c457
will <will@alpha.local>
2021-09-15 13:33:28 -0700

    basic layout for mobile

---
+++ assets/js/careers/careers.js
@@ -0,0 +1,23 @@
+const slides = [
+    {
+        src: "./assets/images/ckc-office-images/1.jpg",
+    },
+    {
+        src: "./assets/images/ckc-office-images/2.jpg",
+    },
+    {
+        src: "./assets/images/ckc-office-images/3.jpg",
+    },
+    {
+        src: "./assets/images/ckc-office-images/4.jpg",
+    },
+    {
+        src: "./assets/images/ckc-office-images/5.jpg",
+    },
+    {
+        src: "./assets/images/ckc-office-images/6.jpg",
+    },
+    {
+        src: "./assets/images/ckc-office-images/7.jpg",
+    },
+]
